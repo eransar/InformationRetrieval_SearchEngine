5a9c35aa4f5701a61c814ede1330a8a149cf16c0
idanf <idanforu@gmail.com>
2018-10-31 20:10:11 +0200

    new parser + enum

--- src/main/java/EnumParse.java
+++ src/main/java/EnumParse.java
@@ -20,18 +20,53 @@ public enum EnumParse {
         public HashSet<String> parse(int i){
             HashSet<String> toReturn = new HashSet<>();
             String[] text = Parser.getTextWithoutDelimeters();
-
+            NumberFormat format = NumberFormat.getInstance(Locale.ENGLISH);
+            Number number = null;
+            try {
+                number = format.parse(text[i]);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            double number_term = number.doubleValue();
             //has a keyword after the word
             if(i+1 < text.length && first_keywords.contains(text[i+1])){
+                //number + value + U.S + Dollar
+                if(i+2<text.length && text[i+2].equals("U.S.") && i+3<text.length && text[i+3].equals("Dollars")){
+                    switch (text[i+1]){
+                        case "million":
+                        break;
+                        case  "billion": number_term =number_term*1000;
+                        break;
+                        case "trillion":number_term = number_term*1000000;
+                        break;
+                    }
+                    toReturn.add(convertDouble(number_term)+" "+"M"+" "+"Dollars");
+                }
+                else{
+                    switch (text[i+1]){
+                        case "Thousand":number_term = number_term/1000;
+                            toReturn.add(convertDouble(number_term) +"K");
+                        break;
+                        case "Million": number_term = number_term/1000000;
+                            toReturn.add(convertDouble(number_term) +"M");
+                            break;
+                        case "Billion":number_term = number_term/1000000000;
+                            toReturn.add(convertDouble(number_term) +"B");
+                            break;
+                        case "percent":
+                        case "percentage":
+                            toReturn.add(convertDouble(number_term) +"%");
+                        break;
+                        case "Dollars":
+                            toReturn.add(transformNumber(number_term));
+                        break;
+                    }
+                }
+            }
+            else if(i+1<text.length && (months().contains(text[i+1].substring(0,1).toUpperCase()+text[i+1].substring(1)) || months().contains(text[i+1].toUpperCase()))){
 
             }
             else{
-                NumberFormat format = NumberFormat.getInstance(Locale.ENGLISH);
-
-                try {
-                    Number number = format.parse(text[i]);
-                    double number_term = number.doubleValue();
-
                     if(number_term >=1000 && number_term <1000000){
                         toReturn.add(convertDouble(number_term/1000)+"K");
                     }
@@ -44,13 +79,7 @@ public enum EnumParse {
                     else{
                          toReturn.add(convertDouble(number_term));
                     }
-
-
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
             }
-
 
             return toReturn;
         }
@@ -66,7 +95,22 @@ public enum EnumParse {
             keywords.add("billion");
             keywords.add("million");
             keywords.add("trillion");
-
+            return keywords;
+        }
+        public ArrayList<String> months(){
+            ArrayList<String> keywords=new ArrayList<String>();
+            keywords.add("january");
+            keywords.add("february");
+            keywords.add("march");
+            keywords.add("april");
+            keywords.add("may");
+            keywords.add("june");
+            keywords.add("july");
+            keywords.add("august");
+            keywords.add("september");
+            keywords.add("october");
+            keywords.add("november");
+            keywords.add("december");
             return keywords;
         }
     },
@@ -77,6 +121,13 @@ public enum EnumParse {
             return toReturn;
         }
     };
+
+    public String transformNumber(double number) {
+        if(number>1000000){
+            return number/1000000+" "+"M"+" "+"Dollars";
+        }
+        return number+" "+"Dollars";
+    }
 
     public abstract HashSet<String> parse(int i);
     public String convertDouble(double d){
