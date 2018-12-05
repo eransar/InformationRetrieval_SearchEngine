b016783c0c10316738d4585879043695bff3b0d1
erans <erans@post.bgu.ac.il>
2018-12-05 17:15:31 +0200

    added javadoc to cityindexer

--- src/main/java/PartA/Stemmer.java
+++ src/main/java/PartA/Stemmer.java
@@ -4,6 +4,9 @@ package PartA;
 import org.tartarus.snowball.SnowballStemmer;
 import org.tartarus.snowball.ext.englishStemmer;
 
+/**
+ * Stemmer class in charge of analyzing the terms and decreasing the dictionary by removing the suffix for terms
+ */
 public class Stemmer {
 
     private SnowballStemmer stemmer = new englishStemmer();
@@ -14,16 +17,19 @@ public class Stemmer {
         return ourInstance;
     }
 
-    public void setCurrent(String term_name){
-        stemmer.setCurrent(term_name);
-    }
 
     public boolean stem(){
         return stemmer.stem();
     }
 
+    //<editor-fold desc="Getter and Setter">
+    public void setCurrent(String term_name){
+        stemmer.setCurrent(term_name);
+    }
+
     public String getCurrent(){
         return stemmer.getCurrent();
     }
+    //</editor-fold>
 
 }
